61c9ad859891095f578ed3e6ef641c2218128641
Benjamin Maggi <benjaminmaggi@gmail.com>
2012-12-11 11:33:42 -0500

    subscription model

---
+++ controllers/subscription.js
@@ -0,0 +1,116 @@
+///////////////////////////////////////////////////////////////////////////////
+// @file         : subscription.js                                           //
+// @summary      : subscription controller                                   //
+// @version      : 0.1                                                       //
+// @project      : Node.JS + Express boilerplate for cloud9 and appFog       //
+// @description  :                                                           //
+// @author       : Benjamin Maggi                                            //
+// @email        : benjaminmaggi@gmail.com                                   //
+// @date         : 12 Dec 2012                                               //
+// ------------------------------------------------------------------------- //
+//                                                                           //
+// @copyright Copyright 2012 Benjamin Maggi, all rights reserved.            //
+//                                                                           //
+//                                                                           //
+// License:                                                                  //
+// This program is free software; you can redistribute it                    //
+// and/or modify it under the terms of the GNU General Public                //
+// License as published by the Free Software Foundation;                     //
+// either version 2 of the License, or (at your option) any                  //
+// later version.                                                            //
+//                                                                           //
+// This program is distributed in the hope that it will be useful,           //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of            //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             //
+// GNU General Public License for more details.                              //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+// Controllers
+var mongoose = require('mongoose');
+
+
+// Load model
+var subscription_schema = require('../models/subscription')
+  , Subscription = mongoose.model('Subscription', subscription_schema);
+  
+///////////////////////////////////////////////////////////////////////////////
+// Route to get all Subscriptions                                            //
+//                                                                           //
+// @param {Object} req                                                       //
+// @param {Object} res                                                       //
+// @param {Object} next                                                      //
+// @return {Object} JSON Collection of Subscriptions                         //
+//                                                                           //
+// @api public                                                               //
+//                                                                           //
+// @url GET /subscription/getAll                                             //
+///////////////////////////////////////////////////////////////////////////////
+
+exports.getAll = function (req, res, next) {
+    
+    res.contentType('application/json');
+    Subscription.find(gotSubscriptions);
+    
+    function gotSubscriptions (err, subscriptions) {
+        if (err) {
+            console.log(err);
+            return next();
+        }
+        var subscriptionsJSON = JSON.stringify(subscriptions);
+        return res.send(subscriptionsJSON);
+    }
+};
+
+///////////////////////////////////////////////////////////////////////////////
+// Route to a specific Subscription                                          //
+//                                                                           //
+// @param {Object} req                                                       //
+// @param {Object} res                                                       //
+// @param {Object} next                                                      //
+// @return {Object} JSON Subscription                                        //
+//                                                                           //
+// @api public                                                               //
+//                                                                           //
+// @url GET /subscription/getById                                            //
+///////////////////////////////////////////////////////////////////////////////
+exports.getById = function (req, res, next) {
+    
+    res.contentType('application/json');
+    Subscription.findById(req.params.id, gotSubscription);
+    
+    function gotSubscription (err, subscription) {
+        if (err) {
+            return next(err);
+        }
+        var subscriptionJSON = JSON.stringify(subscription);
+        return res.send(subscriptionJSON);
+    }
+};
+
+///////////////////////////////////////////////////////////////////////////////
+// Route to remove a Subscription                                            //
+//                                                                           //
+// @param {Object} req                                                       //
+// @param {Object} res                                                       //
+// @param {Object} next                                                      //
+// @return {Object} JSON result                                              //
+//                                                                           //
+// @api public                                                               //
+//                                                                           //
+// @url GET /subscription/remove                                             //
+///////////////////////////////////////////////////////////////////////////////
+exports.remove = function (req, res, next) {
+    
+    res.contentType('application/json');
+    Subscription.remove({_id: req.params.id}, delSubscription);
+    
+    function delSubscription (err) {
+        if (err) {
+            return next(err);
+        }
+        var msgJSON = JSON.stringify({action: 'remove', result: 0});
+        return res.send(msgJSON);
+    }
+};
+
---
+++ models/subscription.js
@@ -0,0 +1,45 @@
+///////////////////////////////////////////////////////////////////////////////
+// FileName     : subscription.js                                            //
+// Version      : 0.1                                                        //
+// Project      : Node.JS + Express boilerplate for cloud9 and appFog        //
+// Author       : Benjamin Maggi                                             //
+// Email        : benjaminmaggi@gmail.com                                    //
+// Date         : 12 Dec 2012                                                //
+// ------------------------------------------------------------------------- //
+//                                                                           //
+//                                                                           //
+// License:                                                                  //
+// This program is free software; you can redistribute it                    //
+// and/or modify it under the terms of the GNU General Public                //
+// License as published by the Free Software Foundation;                     //
+// either version 2 of the License, or (at your option) any                  //
+// later version.                                                            //
+//                                                                           //
+// This program is distributed in the hope that it will be useful,           //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of            //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             //
+// GNU General Public License for more details.                              //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var Subscription = new Schema({
+    name: { type: String, required: true, trim: true },
+    type: String,
+    magic: Number,
+    created: { type: Date, default: Date.now },
+    lastUpdate: { type: Date, default: Date.now },
+    lastAccess: { type: Date, default: Date.now },
+    isReady: Boolean,
+    overview: Boolean,
+    stations: [],
+    humidity: { 
+        value: Number, 
+        dewpoint: Number, 
+        unit: String
+    }
+});
+
+module.exports = mongoose.model('Subscription', Subscription);
