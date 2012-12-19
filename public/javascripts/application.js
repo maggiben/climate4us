1aa7620a8d36db4732076d72e4e4cf6490b3bb9b
Benjamin Maggi <benjaminmaggi@gmail.com>
2012-12-18 20:31:26 -0500

    de casa

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -831,7 +831,87 @@
 ///////////////////////////////////////////////////////////////////////////////
 // jQuery on document ready closure                                          //
 ///////////////////////////////////////////////////////////////////////////////
-$(document).ready(function () {
+/*
+$(document).ready(function(){
+    function initialize() {
+        var mapOptions = {
+        center: new google.maps.LatLng(-34.6036, -58.3817),
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+
+    var input = document.getElementById('new_location');
+    var autocomplete = new google.maps.places.Autocomplete(input);
+
+    autocomplete.bindTo('bounds', map);
+    //autocomplete.setTypes(['geocode']);
+    
+    var infowindow = new google.maps.InfoWindow();
+    var marker = new google.maps.Marker({
+      map: map
+    });
+
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+      infowindow.close();
+      marker.setVisible(false);
+      input.className = '';
+      var place = autocomplete.getPlace();
+      if (!place.geometry) {
+        // Inform the user that the place was not found and return.
+        input.className = 'notfound';
+        return;
+      }
+
+      // If the place has a geometry, then present it on a map.
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+      } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);  // Why 17? Because it looks good.
+      }
+      var image = new google.maps.MarkerImage(
+          place.icon,
+          new google.maps.Size(71, 71),
+          new google.maps.Point(0, 0),
+          new google.maps.Point(17, 34),
+          new google.maps.Size(35, 35));
+      marker.setIcon(image);
+      marker.setPosition(place.geometry.location);
+
+      var address = '';
+      if (place.address_components) {
+        address = [
+          (place.address_components[0] && place.address_components[0].short_name || ''),
+          (place.address_components[1] && place.address_components[1].short_name || ''),
+          (place.address_components[2] && place.address_components[2].short_name || '')
+        ].join(' ');
+      }
+
+      infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+      infowindow.open(map, marker);
+    });
+
+    // Sets a listener on a radio button to change the filter type on Places
+    // Autocomplete.
+    function setupClickListener(id, types) {
+      var radioButton = document.getElementById(id);
+      google.maps.event.addDomListener(radioButton, 'click', function() {
+        autocomplete.setTypes(types);
+      });
+    }
+
+    setupClickListener('changetype-all', []);
+    setupClickListener('changetype-establishment', ['establishment']);
+    setupClickListener('changetype-geocode', ['geocode']);
+  }
+  google.maps.event.addDomListener(window, 'load', initialize);
+});
+*/
+///////////////////////////////////////////////////////////////////////////////
+// jQuery on document ready closure                                          //
+///////////////////////////////////////////////////////////////////////////////
+$(document).ready(function() {
     "use strict";
     
     $("#sites").sortable({
@@ -865,10 +945,12 @@ $(document).ready(function () {
             }, 0);
         }
     });
+    
     $(function initialize() {
-        var myLatlng = new google.maps.LatLng(-34.6036, -58.3817);
+        var myLatlng = new google.maps.LatLng(0, 0) //(-34.6036, -58.3817);
+        var marker;
         var myOptions = {
-          zoom: 8,
+          zoom: 1,
           center: myLatlng,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         }        
@@ -883,11 +965,13 @@ $(document).ready(function () {
         ;
         var infowindow = new google.maps.InfoWindow();
         
+        /*
         var marker = new google.maps.Marker({
             position: myLatlng,
             map: map,
             title: 'Galaconcert'
         });
+        */
         var input = document.getElementById('new_location');         
         var autocomplete = new google.maps.places.Autocomplete(input, {
             types: ["geocode"]
@@ -943,7 +1027,7 @@ $(document).ready(function () {
             infowindow.open(map, marker);
         });
     });
-
+    
     $(window).scroll(function() {
         var a = $(window).height()
         , docHeight = $(document).height()
